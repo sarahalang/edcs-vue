3d1b610620147c679b41548f539846cfe6880679
sarahalang <contact.sarah.a.lang@gmail.com>
2020-10-11 11:45:26 +0200

    deployed functional preview

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <edcs msg="Willkommen zur Clauss Slaby Transformation." />
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import edcs from './components/edcs.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    edcs
   }
 }
 </script>
---
+++ src/components/edcs.vue
@@ -0,0 +1,15 @@
+<template>
+  <h3> Transform EDCS </h3>
+</template>
+
+<script>
+  export default {
+    name: 'edcs',
+  }
+</script>
+
+<style>
+  h3 {
+    margin-bottom: 5%;
+  }
+</style>
